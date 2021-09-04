904db5c7b3e390817b82cf620ac65973af39e575
Seth Baffuor <baffuorakoto137@gmail.com>
2021-09-04 22:20:49 +0000

    Refactored routes and created a controller for endpoints

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
 
-
+const bootcamps = require("./routes/bootcamps");
 //load config vars
 
- dotenv.config({path: "./config/config.env"})
- 
+dotenv.config({ path: "./config/config.env" });
 
- const app = express()
- 
-const PORT = process.env.PORT || 5000
+const app = express();
 
- app.listen( PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
