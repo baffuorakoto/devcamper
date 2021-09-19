b400a5a5b70f9a45f167538c357f5855966b5b4a
Seth Baffuor <baffuorakoto137@gmail.com>
2021-09-19 23:58:41 +0000

    Added login and register routes

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add an name"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please add an email"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email address",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//Encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const errorHandler = require("./middleware/error");
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 const colors = require("colors");
 const connectDB = require("./config/db");
 //load config vars
@@ -33,6 +34,8 @@ app.use(express.static(path.join(__dirname, "public")));
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
